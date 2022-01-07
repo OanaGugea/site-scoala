f1f4fe3615e7942b0714372c3fc450bcf5de24bb
Beni Groza <benigroza@oanas-mbp.home>
2022-01-07 13:56:40 +0200

    add carusel

--- src/components/photo-grid/PhotoGrid.tsx
+++ src/components/photo-grid/PhotoGrid.tsx
@@ -1,6 +1,8 @@
 import frontPart from './assets/front-part.jpeg'
 import backPart from './assets/back-yard.jpeg'
 import {makeStyles} from "@material-ui/core/styles";
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
 const useStyle = makeStyles(() => ({
     imageContainer: {
@@ -8,18 +10,32 @@ const useStyle = makeStyles(() => ({
         justifyContent: 'center'
     },
     photo: {
-        height: '300px',
-        width: '350px',
+        height: '1200px',
+        width: '1200px',
         margin: '0 15px'
+    },
+    photoHeight: {
+        height: '700px'
     }
 }))
+
+// export const PhotoGrid = () => {
+//     const classes = useStyle();
+//     return (
+//         <div className={classes.imageContainer}>
+//             <img src={frontPart} alt="front-part" className={classes.photo}/>
+//             <img src={backPart} alt="back-yard" className={classes.photo}/>
+//         </div>
+//     )
+// }
 
 export const PhotoGrid = () => {
     const classes = useStyle();
     return (
-        <div className={classes.imageContainer}>
-            <img src={frontPart} alt="front-part" className={classes.photo}/>
-            <img src={backPart} alt="back-yard" className={classes.photo}/>
-        </div>
-    )
-}
+    <AwesomeSlider className={classes.photoHeight}>
+      <div><img src={frontPart} alt="front-part"/></div>
+      <div><img src={backPart} alt="back-yard"/></div>
+    </AwesomeSlider>
+    )};
+
+  export default PhotoGrid;
