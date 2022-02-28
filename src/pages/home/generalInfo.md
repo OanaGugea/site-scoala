4d7a8474adbcee01b58c1f610a308ead722ae161
Oana Gugea <oana.gugea@pearson.com>
2022-02-28 15:54:47 +0200

    more news added

--- src/pages/home/generalInfo.md
+++ src/pages/home/generalInfo.md
@@ -6,3 +6,8 @@ optime pentru studiu și dezvoltare. Elevii aparțin comunei Slatina- Timiș și
 Clădirile desemnate procesului educativ (grădiniță, învățământ primar, învățământ gimnazial) au fost recent reabilitate
 și oferă condiții optime pentru studiu. Unitatea de învățământ dispune de teren sintetic pentru activități sportive,
 dar și de sală de sport amenajată de curând.
+
+
+##### Proiect PROF:
+Ministerul Educației, în calitate de beneficiar, implementează, în parteneriat, proiectul necompetitiv sistemic POCU/904/6/25, cod SMIS 146587 – Profesionalizarea carierei didactice – PROF, în conformitate cu precizările din Contractul de finanțare nr. 32811/31.03.2021.
+Informații proiect POCU/904/6/25, cod SMIS 146587 – Profesionalizarea carierei didactice – PROF: [http://ccdhunedoara.ro](http://ccdhunedoara.ro/?a=83)
--- src/pages/news/news-content.ts
+++ src/pages/news/news-content.ts
@@ -11,11 +11,21 @@ export interface INewsContent {
 
 export const newsContentArray: INewsContent[] = [
   {
-    title: "Informare privind gradul de vaccinare în rândul personalului angajat",
+    title:
+      "Informare privind gradul de vaccinare în rândul personalului angajat",
     content:
-      "Potrivit datelor centralizate la nivelul instituției, la data de 01.11.2021, rata generală de vaccinare a ajuns" + 
+      "Potrivit datelor centralizate la nivelul instituției, la data de 01.11.2021, rata generală de vaccinare a ajuns" +
       "la 65% din totalul celor 23 de angajați ai unității de învățământ. Raportat la personalul care deține funcții" +
       "de conducere, ponderea celor vaccinați este de 100%.",
+  },
+  {
+    title: "Copii pentru copii!",
+    content:
+      "Campania derulată de radio Reșița primeste sprijin din partea Inspectoratului Școlar Județean. Începând de luni, 28 februarie, în toate unitățile de învățământ din Caraș Severin, dascălii, elevii și părinții pot dona produse de igienă, alimente neperisabile, saci de dormit, pături și mai ales produse pentru copii pentru refugiații ucraineni.\n" +
+      "Radio România Reșița va difuza de luni scurte reportaje realizate cu elevi care sunt îndemnați să transmită mesajele lor către copiii sosiți la noi în țară, inclusiv cu elevii din comunitatea ucraineană care se pot face înțeleși de copiii refugiați.\n" +
+      "Pentru mediul online propunem o serie de videoclipuri scurte realizate de copii cu desene, îndemnuri, prezentarea locurilor frumoase din România. \n" +
+      "Tik-Tok, Instagram, Snapchat sunt rețelele cele mai utilizate de copii ceea ce înseamnă că radioul ar trebui să vină cu filmulețe care pot fi sharuite.\n" +
+      "Când fugi din calea războiului, ultimul lucru pe care îl iei cu tine sunt jucăriile copiilor. O jucărie primită în dar la intrarea într-o țară străină este cel mai ușor mod de a aduce un zâmbet pe chipul unui copil speriat.",
   },
   // {
   //   title: "Post vacant",
